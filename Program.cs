dbc71bb303e4b4f313550fa0d49b800d3fe63d58
Glyson <kauaglyson@gmail.com>
2022-11-16 22:24:19 -0300

    Configuring the MYSQL Database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using AulaEntityFramework.Context;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+
+// armazenando a connection string (configurada no app settings) nessa variável
+var connectionStringMysql = builder.Configuration.GetConnectionString("ConexaoPadrao");
+// configurando o banco MYSQL
+builder.Services.AddDbContext<AgendaContext>(
+    options => options.UseMySql(
+        connectionStringMysql,
+        ServerVersion.Parse("8.0.31-Oubuntu0.22.04.1")
+    )
+);
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
