5a836d13eaef0a4409ac12f529d50fa75a28895e
Glyson <kauaglyson@gmail.com>
2022-11-16 21:17:55 -0300

    Creating database named Agenda and a entity named Contatos

--- Context/AgendaContext.cs
+++ Context/AgendaContext.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using AulaEntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
 namespace AulaEntityFramework.Context
 {
-    public class AgendaContext
+    public class AgendaContext : DbContext
     {
-        
+        // construtor que define as config de conexão do DB (context)
+        // a conexão é passada por parâmetro e em seguida é enviada para o base(DbContext)
+        public AgendaContext (DbContextOptions<AgendaContext> options) : base (options)
+        {
+            // construtor é vazio pois só irá receber os parâmetros
+        }
+
+        // transformando a classe Contato em uma entidade (uma tabela no DB) chamada Contatos.
+        public DbSet <Contato> Contatos { get; set; }
     }
 }
