6746c0226e4476e326c0616dcf65c7c261a53577
Glyson <kauaglyson@gmail.com>
2022-11-17 23:00:10 -0300

    get data from Database via HTTP GET

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -12,6 +12,7 @@ namespace AulaEntityFramework.Controllers
     [Route("[controller]")]
     public class ContatoController : ControllerBase
     {
+        // recebe o banco de dados (o valor é passado no construtor)
         private readonly AgendaContext _context;
 
         // construtor que recebe o banco (context)
@@ -25,6 +26,20 @@ namespace AulaEntityFramework.Controllers
         {
             _context.Add(contato);
             _context.SaveChanges();
+            return Ok(contato);
+        }
+
+        // as chaves especifica que vai receber um parâmetro.
+        // o endpoint seria /Contato/id
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // o Contatos é o dbset (a tabela no BD)
+            var contato = _context.Contatos.Find(id);
+            // verificação do contato
+            if (contato == null)
+                return NotFound();
+
             return Ok(contato);
         }
     }
